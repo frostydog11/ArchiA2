758532ac281157cc352bf0de63b19d3df02637d4
frostydog11 <arankin730@gmail.com>
2026-02-12 08:37:35 -0400

    files made, find functions

---
+++ a1solution/a1solution/src/A1_AutoDetail.App/Persistence/Dao/BookingDao.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using A1_AutoDetail.App.Domain.Contracts;
+using A1_AutoDetail.App.Domain.Entities;
+using A1_AutoDetail.App.Persistence;
+
+namespace A1_AutoDetail.App.Persistence.Dao
+{
+    public class BookingDao : IBookingDao
+    {
+        private readonly AppDbContext _db;
+
+        public BookingDao(AppDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public Booking FindBooking(int bookingId)
+        {
+            return _db.Bookings.Find(bookingId);
+        }
+
+        public Customer FindCustomer(int customerId)
+        {
+            return _db.Customers.Find(customerId);
+        }
+
+        public DetailService FindDetailService(int detailId)
+        {
+            return _db.DetailServices.Find(detailId);
+        }
+
+        public TimeSlot FindTimeSlot (int timeSlotId)
+        {
+            return _db.TimeSlots.Find(timeSlotId);
+        }
+    }
+}
